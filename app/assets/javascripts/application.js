f16204fb2942a2fe935954840e4a2db278aea846
Gerard Devine <g.devine@uws.edu.au>
2014-07-16 09:23:20 +1000

    Update Gemfile.lock for Heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
-//= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
+//= require bootstrap
+//= require jquery
 //= require turbolinks
 //= require jquery.nested-fields
 //= require_tree .

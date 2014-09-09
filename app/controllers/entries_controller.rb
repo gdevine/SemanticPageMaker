81f6ec489355efe24f86ecac7fe3b9f3e47d3269
Gerard Devine <g.devine@uws.edu.au>
2014-09-09 12:36:34 +1000

    Index page seperated into entity types

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,42 @@
 class EntriesController < ApplicationController
-  before_action :signed_in_user, only: [:index, :new, :show, :update, :edit, :create, :destroy]
+  before_action :signed_in_user, only: [:new, :update, :edit, :create, :destroy]
   before_action :correct_user,   only: :destroy
   
-  def index   
+  def index        
+    # Index returns entries broken down by entity type     
+      
     @entries = Entry.paginate(page: params[:page])
+    
+    # create array to track already found entity types
+    entities_completed = entity_entries = []
+    # retrieve all entries
+    all_entries = Entry.all
+    # create a master array to contain all sorted entries
+    @sorted_entries = []
+    all_entries.each do |entry|
+      my_entity_type = Entity.find(entry.entity_id).name
+      if !entities_completed.include?(my_entity_type)
+        # entity type has not been found as of yet so create a new hash
+        entity_collection = {"entity_type" => my_entity_type, "entries" => []}
+        # create a mini hash for this entry
+        entry_hash = {"slug" => entry.slug, "url" => entry_path(entry.id)}
+        # add entry mini hash to entity_collection
+        entity_collection["entries"] << entry_hash 
+        # add me as a new member of the sorted_entries master array
+        @sorted_entries << entity_collection
+        # add entity type to those found already (so that future instance are added to this one)
+        entities_completed << my_entity_type
+      else
+        # retrieve the matching existing entity_collection
+        entity_collection = @sorted_entries.find {|collection| collection["entity_type"] == my_entity_type }
+        # create a mini hash for this entry
+        entry_hash = {"slug" => entry.slug, "url" => entry_path(entry.id)}
+        # add entry mini hash to entity_collection
+        entity_collection["entries"] << entry_hash          
+      end
+        
+    end
+      
     @entities = Entity.all
   end
   
@@ -80,6 +113,8 @@ class EntriesController < ApplicationController
     
     @entity = Entity.find(@entry.entity_id)
     @entities = Entity.all
+    
+    render :layout => 'entry_layout'
   end
   
   def destroy
@@ -94,6 +129,7 @@ class EntriesController < ApplicationController
                                     :entry_id, 
                                     :field_id, 
                                     :textblock,
+                                    :slug,
                                     field_instances_attributes: [:id, :entry_id, :field_id, :exposeAs, :answer, :_destroy],
                                     entity_instances_attributes: [:id, :entry_id, :link_id, :exposeAs, :property, :_destroy])
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901214440) do
+ActiveRecord::Schema.define(version: 20140908075457) do
 
   create_table "comfy_cms_blocks", force: true do |t|
     t.string   "identifier",                      null: false
@@ -166,6 +166,7 @@ ActiveRecord::Schema.define(version: 20140901214440) do
     t.datetime "updated_at"
     t.integer  "creator_id"
     t.text     "textblock"
+    t.string   "slug"
   end
 
   create_table "field_instances", force: true do |t|
